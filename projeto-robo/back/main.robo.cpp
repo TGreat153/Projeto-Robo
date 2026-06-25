101bdac862a3af88341e64ec1bbabb0c39b7a1a7
Joao Lucas do Nascimento Fonseca <jlzin81@gmail.com>
2026-06-25 10:28:25 -0300

    codigo corrigido

--- projeto-robo/back/main.robo.cpp
+++ projeto-robo/back/main.robo.cpp
@@ -1,20 +1,16 @@
 #include <Arduino.h>
-#include <WiFi.h> // lib do wifi
+#include <WiFi.h>
+#include "esp_camera.h"
+#include "esp_http_server.h"
 
 // --- Config rede ---
-const char* WiFi_SSID = "robo FP";
-const char* WiFi_PASS = "robo123456";
-
-// Server na porta 80
-WiFiServer servidor(80);
+const char* ssid = "NOME_DA_REDE_AQUI";
+const char* password = "SENHA_AQUI";
 
 // --- Pinos Ponte H ---
-// Esquerda
 #define PINO_VEL_ESQ 2 
 #define PINO_ESQ_FRENTE 13    
 #define PINO_ESQ_TRAS 14      
-
-// Direita
 #define PINO_VEL_DIR 12
 #define PINO_DIR_FRENTE 15
 #define PINO_DIR_TRAS 33
@@ -25,15 +21,36 @@ WiFiServer servidor(80);
 #define CANAL_PWM_ESQ 0
 #define CANAL_PWM_DIR 1
 
+// --- Pinos Camera 
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
 
-// --- Funcoes de Movimento ---
+httpd_handle_t servidor = NULL;
+
+
+
+// FUNCOES DE MOVIMENTO (CALIBRADAS)
+/
+
 void parar_motores() {
   ledcWrite(CANAL_PWM_ESQ, 0);
   ledcWrite(CANAL_PWM_DIR, 0);
-
   digitalWrite(PINO_ESQ_FRENTE, LOW);
   digitalWrite(PINO_ESQ_TRAS, LOW);
-
   digitalWrite(PINO_DIR_FRENTE, LOW);
   digitalWrite(PINO_DIR_TRAS, LOW);
 }
@@ -41,13 +58,9 @@ void parar_motores() {
 void andar_frente(int velocidad) {
   digitalWrite(PINO_ESQ_FRENTE, HIGH);
   digitalWrite(PINO_ESQ_TRAS, LOW);
-
   digitalWrite(PINO_DIR_FRENTE, HIGH);
   digitalWrite(PINO_DIR_TRAS, LOW);
-
-  // calibracao: reduz motor esquerdo em ~15% pra corrigir desvio do chassi
   int velocidade_calibrada_esq = velocidad * 0.85; 
-
   ledcWrite(CANAL_PWM_ESQ, velocidade_calibrada_esq);
   ledcWrite(CANAL_PWM_DIR, velocidad);
 }
@@ -55,10 +68,8 @@ void andar_frente(int velocidad) {
 void andar_tras(int velocidade) {
   digitalWrite(PINO_ESQ_FRENTE, LOW);
   digitalWrite(PINO_ESQ_TRAS, HIGH);
-  
   digitalWrite(PINO_DIR_FRENTE, LOW);
   digitalWrite(PINO_DIR_TRAS, HIGH);
-
   ledcWrite(CANAL_PWM_ESQ, velocidade);
   ledcWrite(CANAL_PWM_DIR, velocidade);
 }
@@ -66,10 +77,8 @@ void andar_tras(int velocidade) {
 void girar_esquerda(int velocidade) {
   digitalWrite(PINO_ESQ_FRENTE, LOW);
   digitalWrite(PINO_ESQ_TRAS, HIGH);
-  
   digitalWrite(PINO_DIR_FRENTE, HIGH);
   digitalWrite(PINO_DIR_TRAS, LOW);
-
   ledcWrite(CANAL_PWM_ESQ, velocidade);
   ledcWrite(CANAL_PWM_DIR, velocidade);
 }
@@ -77,20 +86,86 @@ void girar_esquerda(int velocidade) {
 void girar_direita(int velocidade) {
   digitalWrite(PINO_ESQ_FRENTE, HIGH);
   digitalWrite(PINO_ESQ_TRAS, LOW);
-  
   digitalWrite(PINO_DIR_FRENTE, LOW);
   digitalWrite(PINO_DIR_TRAS, HIGH);
-
   ledcWrite(CANAL_PWM_ESQ, velocidade);
   ledcWrite(CANAL_PWM_DIR, velocidade);
 }
 
 
-// --- Setup ---
+// ROTEAMENTO HTTP (MOTORES E VIDEO)
+
+// Intercepta os comandos de movimento
+esp_err_t controle_handler(httpd_req_t *req) {
+  char* rota = (char*)req->uri;
+  
+  if (strstr(rota, "/frente")) andar_frente(255);
+  else if (strstr(rota, "/tras")) andar_tras(255);
+  else if (strstr(rota, "/esquerda")) girar_esquerda(255);
+  else if (strstr(rota, "/direita")) girar_direita(255);
+  else if (strstr(rota, "/parar")) parar_motores();
+  
+  // Resposta padrao para o Python nao travar
+  httpd_resp_set_type(req, "text/plain");
+  return httpd_resp_send(req, "OK", 2);
+}
+
+// Protocolo MJPEG padrao para o OpenCV conseguir puxar o video
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+esp_err_t video_handler(httpd_req_t *req) {
+  camera_fb_t * fb = NULL;
+  esp_err_t res = ESP_OK;
+  size_t _jpg_buf_len = 0;
+  uint8_t * _jpg_buf = NULL;
+  char * part_buf[64];
+
+  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+  if(res != ESP_OK) return res;
+
+  while(true){
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Falha na captura da camera");
+      res = ESP_FAIL;
+    } else {
+      _jpg_buf_len = fb->len;
+      _jpg_buf = fb->buf;
+    }
+    if(res == ESP_OK){
+      size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+    }
+    if(res == ESP_OK){
+      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+    }
+    if(res == ESP_OK){
+      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+    }
+    if(fb){
+      esp_camera_fb_return(fb);
+      fb = NULL;
+      _jpg_buf = NULL;
+    } else if(_jpg_buf){
+      free(_jpg_buf);
+      _jpg_buf = NULL;
+    }
+    if(res != ESP_OK) break;
+  }
+  return res;
+}
+
+//
+// SETUP
+//
+
 void setup() {
-  // serial pra pegar o ip no monitor
   Serial.begin(115200);
 
+  // 1. Setup Pinos Motor
   pinMode(PINO_ESQ_FRENTE, OUTPUT);
   pinMode(PINO_ESQ_TRAS, OUTPUT);
   pinMode(PINO_DIR_FRENTE, OUTPUT);
@@ -98,79 +173,81 @@ void setup() {
 
   ledcSetup(CANAL_PWM_ESQ, FREQUENCIA_PWM, RESOLUCAO_PWM);
   ledcAttachPin(PINO_VEL_ESQ, CANAL_PWM_ESQ);
-
   ledcSetup(CANAL_PWM_DIR, FREQUENCIA_PWM, RESOLUCAO_PWM);
   ledcAttachPin(PINO_VEL_DIR, CANAL_PWM_DIR);
-
   parar_motores();
 
-  // conecta wifi
-  Serial.print("Conectando em: ");
-  Serial.println(WiFi_SSID);
-  WiFi.begin(WiFi_SSID, WiFi_PASS);
+  // 2. Setup Camera
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_2;
+  config.ledc_timer = LEDC_TIMER_1;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  
+  // QVGA = 320x240 (Leve, nao trava)
+  config.frame_size = FRAMESIZE_QVGA; 
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
 
-  // trava aqui ate conectar
+  if(esp_camera_init(&config) != ESP_OK){
+    Serial.println("Erro critico: Hardware da camera falhou.");
+    return;
+  }
+
+  // espelha a imagem caso o OpenCV receba invertido
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_vflip(s, 0);   
+  s->set_hmirror(s, 0); 
+
+  // 3. Setup Rede
+  WiFi.begin(ssid, password);
+  Serial.print("Conectando Wi-Fi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
-  // mostra ip pra equipe
-  Serial.println("\nConectado!");
-  Serial.print("IP: ");
+  Serial.println("");
+  Serial.print("IP da Camera e Comandos: ");
   Serial.println(WiFi.localIP());
 
-  servidor.begin();
+  // 4. Inicia o Servidor HTTP Assincrono
+  httpd_config_t config_server = HTTPD_DEFAULT_CONFIG();
+  config_server.server_port = 80;
+
+  httpd_uri_t url_video = { .uri = "/stream", .method = HTTP_GET, .handler = video_handler, .user_ctx = NULL };
+  httpd_uri_t url_frente = { .uri = "/frente", .method = HTTP_GET, .handler = controle_handler, .user_ctx = NULL };
+  httpd_uri_t url_tras = { .uri = "/tras", .method = HTTP_GET, .handler = controle_handler, .user_ctx = NULL };
+  httpd_uri_t url_esq = { .uri = "/esquerda", .method = HTTP_GET, .handler = controle_handler, .user_ctx = NULL };
+  httpd_uri_t url_dir = { .uri = "/direita", .method = HTTP_GET, .handler = controle_handler, .user_ctx = NULL };
+  httpd_uri_t url_parar = { .uri = "/parar", .method = HTTP_GET, .handler = controle_handler, .user_ctx = NULL };
+
+  if (httpd_start(&servidor, &config_server) == ESP_OK) {
+    httpd_register_uri_handler(servidor, &url_video);
+    httpd_register_uri_handler(servidor, &url_frente);
+    httpd_register_uri_handler(servidor, &url_tras);
+    httpd_register_uri_handler(servidor, &url_esq);
+    httpd_register_uri_handler(servidor, &url_dir);
+    httpd_register_uri_handler(servidor, &url_parar);
+  }
 }
 
-
-// --- Loop Principal ---
 void loop() {
-  // checa se tem cliente mandando requisicao
-  WiFiClient cliente = servidor.available();
-
-  if (cliente) {
-    String linha_requisicao = "";
-
-    while (cliente.connected()) {
-      if (cliente.available()) {
-        char c = cliente.read();
-        linha_requisicao += c;
-
-        // quebra de linha indica fim do request
-        if (c == '\n') {
-          if (linha_requisicao.length() == 1) {
-            // header http padrao pro python nao dar timeout
-            cliente.println("HTTP/1.1 200 OK");
-            cliente.println("Content-Type: text/plain");
-            cliente.println("Connection: close");
-            cliente.println();
-            cliente.println("OK");
-            break;
-          }
-
-          // rotas de controle (forca no maximo)
-          if (linha_requisicao.indexOf("GET /frente") >= 0) {
-            andar_frente(255);
-          } 
-          else if (linha_requisicao.indexOf("GET /tras") >= 0) {
-            andar_tras(255);
-          } 
-          else if (linha_requisicao.indexOf("GET /esquerda") >= 0) {
-            girar_esquerda(255);
-          } 
-          else if (linha_requisicao.indexOf("GET /direita") >= 0) {
-            girar_direita(255);
-          } 
-          else if (linha_requisicao.indexOf("GET /parar") >= 0) {
-            parar_motores();
-          }
-
-          linha_requisicao = "";
-        }
-      }
-    }
-    // derruba a conexao pra liberar o loop pro proximo comando
-    cliente.stop();
-  }
+  // Loop vazio. O FreeRTOS processa a camera e a rede nativamente em background.
+  delay(10000); 
 }
