c90563277067efc2f6d56c2a5968d3c63fd909fe
TGreat153 <tarciodesantana2013@gmail.com>
2026-07-04 15:35:19 -0300

    erro 3 corrigido

--- projeto-robo/back/main.py
+++ projeto-robo/back/main.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import utils
 import threading
-from concurrent.futures import ThreadPoolExecutor
 
 app = Flask(__name__)
 cors = CORS(app, resources={r'/api/*': {"origins": "http://localhost:5173"}})
@@ -16,37 +15,42 @@ def receber_data_manual():
     value = utils.manual(data)
 
     return jsonify({
-            'message': f'O robo andou: {value}',
-            'result': value
-        })
+        'message': f'O robo andou: {value}',
+        'result': value
+    })
 
 
 @app.route("/api/robo/auto/start", methods=["POST"])
 def receber_data_auto():
     global thread
     data = request.get_json()
+    
     if thread and thread.is_alive():
         return jsonify({
-                'message': f'Erro!',
-                'result': "Aparte Parar"
-            })
+            'message': 'Erro!',
+            'result': 'O robô já está em modo automático. Aperte Parar primeiro.'
+        }), 400
+
     thread_stop.clear()
-    with ThreadPoolExecutor() as executor:
-        futuro = executor.submit(utils.auto, data, thread_stop)
-        result = futuro.result()
+    
+    thread = threading.Thread(target=utils.auto, args=(data, thread_stop))
+    thread.start()
         
     return jsonify({
-            'message': 'Sucesso!',
-        })
+        'message': 'Modo automático iniciado com sucesso!',
+    })
+
 
 @app.route("/api/robo/auto/stop", methods=["POST"])
 def stop_loop():
     global thread
+    
     thread_stop.set()
 
     return jsonify({
         'message': 'Comando de parada enviado!',
     })
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
--- projeto-robo/frontend/projeto-robo/src/components/about/About.jsx
+++ projeto-robo/frontend/projeto-robo/src/components/about/About.jsx
@@ -2,7 +2,11 @@ function About() {
   return (
     <div className="text_body">
         <h1>Equipe</h1>
-        <p>Alunos</p>
+        <p>Alunos:</p>
+        <p>João Lucas</p>
+        <p>Matheus Gonçaves</p>
+        <p>Nirlon Alexandre</p>
+        <p>Tarcio José</p>
         <h1>Objetivo</h1>
         <p>Este é um trabalho da universidade de Pernambuco Campus Benfica. Do curso de engenharia da Computação. Seu objetivo treinar habilidades do robô!</p>
     </div>
